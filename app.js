665304312571dfc7ffdc4549bb28ba24feed1129
milanju <Milan.Jurhan@live.de>
2015-07-09 08:20:28 +0200

    Add race support & links

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
 $(document).ready(function() {
   $(".app-content").hide();
-
+  console.log("Hello!");
   var ladderAPI = "https://eu.api.battle.net/sc2/ladder/grandmaster?locale=en_GB&apikey=hqcmbnfhfr6xuzmt29yrkcy3a23ekjz2";
   var alias;
+  console.log("...");
   $.getJSON('https://dl.dropboxusercontent.com/s/tyhsuf2poddp89b/alias.json').done (function(data) {
-  alias = data;
-  $.getJSON(ladderAPI).done(function(data) {
-      var id;
-      var rank = 1;
-      var region;
-      var name;
-      var wins;
-      var losses;
-      var points;
-      $.each(data.ladderMembers, function(i, item) {
-        region = "EU";
-        name = item.character.displayName;
-        id = item.character.id;
-        wins = item.wins;
-        losses = item.losses;
-        points = item.points;
-        $.each(alias.exposedBarcodes, function(j, jtem) {
-          if(item.character.id === jtem.id) {
-            name += " // " + jtem.alias;
+    console.log(".........");
+    alias = data;
+    console.log("1");
+    $.getJSON(ladderAPI).done(function(data) {
+        console.log("0");
+        var id;
+        var rank = 1;
+        var region;
+        var name;
+        var wins;
+        var losses;
+        var points;
+        var race;
+        console.log("pls");
+        $.each(data.ladderMembers, function(i, item) {
+          region = "EU";
+          name = item.character.displayName;
+          console.log(name);
+          id = item.character.id;
+          wins = item.wins;
+          losses = item.losses;
+          points = item.points;
+          switch (item.favoriteRaceP1) {
+            case "TERRAN":
+              race = "terran";
+              break;
+            case "ZERG":
+              race = "zerg";
+              break;
+            case "PROTOSS":
+              race = "protoss";
+              break;
+            case "RANDOM":
+              race ="random";
+              break;
           }
+          $.each(alias.exposedBarcodes, function(j, jtem) {
+            if(item.character.id === jtem.id) {
+              name += " <div class='aka'>aka</div> " + jtem.alias;
+            }
+          });
+          name = "<a href='http://eu.battle.net/sc2/en" + item.character.profilePath + "' target='_blank'>" + name + "</a>";
+          name = "<img src='images/" + race + ".png' alt='" + race + "'>" + name;
+          console.log("appending");
+          $("<tr><td id='rank-th'>" + rank
+          + "</td><td class='mdl-data-table__cell--non-numeric'>" + region
+          + "</td><td class='mdl-data-table__cell--non-numeric'>" + name
+          + "</td><td>" + wins
+          + "</td><td>" + losses
+          + "</td><td>" + points
+          + "</td></tr>").appendTo("#ladder-table-body");
+          console.log("appended");
+          rank++;
         });
-        $("<tr><td id='rank-th'>" + rank + "</td><td class='mdl-data-table__cell--non-numeric'>" + region + "</td><td class='mdl-data-table__cell--non-numeric'>" + name + "</td><td>"
-          + wins + "</td><td>" + losses + "</td><td>" + points + "</td></tr>")
-          .appendTo("#ladder-table-body");
-        rank++;
+        $(".mdl-spinner").hide();
+        $(".app-content").fadeIn();
       });
-      $(".mdl-spinner").hide();
-      $(".app-content").fadeIn();
-    });
   });
 
 });
