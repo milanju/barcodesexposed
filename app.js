e67c5ab7f9a8e6ad8fabace6f3838493d122715d
milanju <Milan.Jurhan@live.de>
2015-07-09 03:57:36 +0200

    Move js logic to app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+var ladderAPI = "https://eu.api.battle.net/sc2/ladder/grandmaster?locale=en_GB&apikey=hqcmbnfhfr6xuzmt29yrkcy3a23ekjz2";
+var alias;
+$.getJSON('https://dl.dropboxusercontent.com/s/tyhsuf2poddp89b/alias.json').done (function(data) {
+alias = data;
+$.getJSON(ladderAPI)
+  .done(function( data ) {
+    var name;
+    $.each( data.ladderMembers, function(i, item) {
+      name = item.character.displayName;
+      $.each(alias.exposedBarcodes, function(j, jtem) {
+        if(item.character.id === jtem.id) {
+          name += " // " + jtem.alias;
+        }
+      });
+      $( "<p>" + name + "</p>" ).appendTo( "#ladder" );
+    });
+  });
+});
