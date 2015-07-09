55f8438b3da528e6ae5e166608fe2a1007a547f8
milanju <Milan.Jurhan@live.de>
2015-07-09 06:34:08 +0200

    Add Style

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ var ladderAPI = "https://eu.api.battle.net/sc2/ladder/grandmaster?locale=en_GB&a
 var alias;
 $.getJSON('https://dl.dropboxusercontent.com/s/tyhsuf2poddp89b/alias.json').done (function(data) {
 alias = data;
-$.getJSON(ladderAPI)
-  .done(function( data ) {
+$.getJSON(ladderAPI).done(function(data) {
+    var id;
+    var rank = 1;
+    var region;
     var name;
-    $.each( data.ladderMembers, function(i, item) {
+    var wins;
+    var losses;
+    var points;
+    $.each(data.ladderMembers, function(i, item) {
+      region = "EU";
       name = item.character.displayName;
+      id = item.character.id;
+      wins = item.wins;
+      losses = item.losses;
+      points = item.points;
       $.each(alias.exposedBarcodes, function(j, jtem) {
         if(item.character.id === jtem.id) {
           name += " // " + jtem.alias;
         }
       });
-      $( "<p>" + name + "</p>" ).appendTo( "#ladder" );
+      $("<tr><td id='rank-th'>" + rank + "</td><td class='mdl-data-table__cell--non-numeric'>" + region + "</td><td class='mdl-data-table__cell--non-numeric'>" + name + "</td><td>"
+        + wins + "</td><td>" + losses + "</td><td>" + points + "</td></tr>")
+        .appendTo("#ladder-table-body");
+      rank++;
     });
   });
 });
