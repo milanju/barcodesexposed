b2967bdf512df6f95063b7eb47c8d953954d5ced
unknown <milan.jurhan@live.de>
2015-11-21 11:38:51 +0100

    update ladder id for lotv

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".app-content").hide();
-  var ladderAPI = "https://eu.api.battle.net/sc2/ladder/grandmaster?locale=en_GB&apikey=hqcmbnfhfr6xuzmt29yrkcy3a23ekjz2";
+  var ladderAPI = "https://eu.api.battle.net/sc2/ladder/176680?locale=en_GB&apikey=hqcmbnfhfr6xuzmt29yrkcy3a23ekjz2";
   var alias;
   $.getJSON('https://dl.dropboxusercontent.com/s/tyhsuf2poddp89b/alias.json').done (function(data) {
     alias = data;
